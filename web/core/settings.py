afaee57324b929d9e9af4c96831df3aa3c89ca52
kevin <kevinherbas7@gmail.com>
2024-10-17 18:46:38 +0200

    feature: production ready

--- web/core/settings.py
+++ web/core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-i@$d_^ws0^_v@-r$ocow0s27ih(y5bq$xktfy$5vz!gs6#=orf"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", True)
+DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1")
 
 ALLOWED_HOSTS = ["127.0.0.1", os.getenv("HOST_DOMAIN", "localhost")]
 
